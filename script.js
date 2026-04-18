0ad2cbb9839bf07866a116a522823253890f1163
Borcho <alexbdelcarmen@gmail.com>
2026-04-18 02:31:31 -0600

    add slider to move circle

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d"); 
+var slider = document.getElementById("circleMover")
 
-ctx.beginPath(); 
-ctx.arc(95, 50, 40, 0, 2 * Math.PI); 
-ctx.stroke(); 
+
+function drawCircle(x) {
+    ctx.beginPath(); 
+    ctx.arc(95 + x, 50, 40, 0, 2 * Math.PI); 
+    ctx.stroke(); 
+}
+
+function clearCanvas(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+drawCircle(0);
+
+slider.addEventListener("click", () => {
+    clearCanvas()
+    var sliderValue = parseInt(slider.value)
+    drawCircle(sliderValue)
+})
