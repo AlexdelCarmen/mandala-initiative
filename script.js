ecd3d749c34219c2231ba4efb223e416d01d2a2d
Borcho <alexbdelcarmen@gmail.com>
2026-04-19 21:32:12 -0600

    Add input for number of petals

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d"); 
+var numberOfPetals = document.getElementById("petals");
+
+startingPointX = 300;
+startingPointY = 400;
 
 const radio = 45;
 
-var petals = 6;
+var petals = numberOfPetals.value;
 
 function drawCircle(x, y) {
     ctx.beginPath(); 
@@ -15,14 +19,19 @@ function clearCanvas(){
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 }
 
-drawCircle(200, 100);
+
 
 function rosette(circleX, circleY, currentPetal) {
     pointX = radio * Math.cos((radio * currentPetal)) + circleX;
     pointY = radio * Math.sin((radio * currentPetal)) + circleY; 
 }
 
-for (let i = 0; i < petals; i++) {
-    rosette(200, 100, i);
-    drawCircle(pointX, pointY);
-}
+numberOfPetals.addEventListener("change", function() {
+    petals = numberOfPetals.value;
+    clearCanvas(); 
+    drawCircle(startingPointX, startingPointY);
+    for (let i = 0; i < petals; i++) {
+        rosette(startingPointX, startingPointY, i);
+        drawCircle(pointX, pointY);
+    }
+})
