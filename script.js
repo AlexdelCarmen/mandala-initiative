c5eeb75654cfefcfcdf25156e9d16ee9b1d2a16a
Borcho <alexbdelcarmen@gmail.com>
2026-04-23 12:26:31 -0600

    Add layer randomization hard-coded

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d"); 
-
+var ctx = canvas.getContext("2d");
+var layers = 4;
 
 startingPointX = canvas.width / 2;
 startingPointY = canvas.height / 2;
@@ -8,33 +8,33 @@ startingPointY = canvas.height / 2;
 const radio = 45;
 
 function drawCircle(x, y) {
-    ctx.beginPath(); 
-    ctx.arc(x, y, radio, 0, 2 * Math.PI); 
-    ctx.stroke(); 
+  ctx.beginPath();
+  ctx.arc(x, y, radio, 0, 2 * Math.PI);
+  ctx.stroke();
 }
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath(); 
-    ctx.moveTo(x1, y1); 
-    ctx.lineTo(x2, y2); 
-    ctx.stroke(); 
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
 }
 
-function clearCanvas(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+const capas = [{ radio: 250, repeticiones: 8 }];
 
 function drawMandala(cx, cy) {
-  const capas = [
-    { radio: 50,  repeticiones: 6 },
-    { radio: 100, repeticiones: 14 },
-    { radio: 150, repeticiones: 10  },
-    { radio: 200, repeticiones: 8  },
-  ];
+  for (let i = 1; i <= layers; i++) {
+    var radio = Math.floor(Math.random() * 201) + 50; // Radio aleatorio entre 50 y 250
+    var repeticiones = Math.floor(Math.random() * 12) + 6; // Repeticiones aleatorias entre 6 y 18
+    capas.push({ radio: radio, repeticiones: repeticiones });
+  }
 
   for (const capa of capas) {
     for (let i = 0; i < capa.repeticiones; i++) {
-      const angulo = (2 * Math.PI / capa.repeticiones) * i;
+      const angulo = ((2 * Math.PI) / capa.repeticiones) * i;
       const x = cx + capa.radio * Math.cos(angulo);
       const y = cy + capa.radio * Math.sin(angulo);
       drawLine(cx, cy, x, y);
